cb8777b1d192ecc494d98d5e5fdd120346c5ec40
Alexsandr <alexnem@inbox.ru>
2021-06-05 21:53:56 +0300

    add custom _document, styles; refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
 import {AppProps} from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>
+        Top App
+      </title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+    </>;
 }
 
 export default MyApp;
