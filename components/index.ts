8896788f6cae729aaf3666add6addffd9454038d
Alexsandr <alexnem@inbox.ru>
2021-06-12 19:44:29 +0300

    add Tag component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './Paragraph/Paragraph';
+export * from './Rating/Rating';
+export * from './Tag/Tag';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import {Paragraph} from "../components";
+import {Paragraph, Rating} from "../components";
+import {useState} from "react";
 
 export default function Home(): JSX.Element {
+    const [rating, setRating] = useState(2);
   return (
     <div>
       Hello World
         <Paragraph size="l">
             Hi
         </Paragraph>
+        <Rating rating={rating} setRating={setRating} isEditable={true} />
     </div>
   );
 }
