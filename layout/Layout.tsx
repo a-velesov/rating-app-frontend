b2bd53b21dcc26e787f607c329e3283376e33ef4
Alexsandr <alexnem@inbox.ru>
2021-06-13 14:32:26 +0300

    add styles for layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,19 +2,18 @@ import {LayoutProps} from "./Layout.props";
 import {Header} from "./header/Header";
 import {Footer} from "./footer/Footer";
 import {Sidebar} from "./sidibar/Sidebar";
+import styles from './Layout.module.css';
 import {FunctionComponent} from "react";
 
 const Layout = ({children}: LayoutProps): JSX.Element => {
-    return <>
-        <Header />
-        <div>
-            <Sidebar />
-            <div>
-                {children}
-            </div>
+    return <div className={styles.wrapper}>
+        <Header className={styles.header} />
+        <Sidebar className={styles.sidebar} />
+        <div className={styles.body}>
+            {children}
         </div>
-        <Footer />
-    </>;
+        <Footer className={styles.footer} />
+    </div>;
 };
 
 export const withLayout = <T extends Record<string, unknown>> (Component: FunctionComponent<T>) => {
--- layout/footer/Footer.tsx
+++ layout/footer/Footer.tsx
@@ -1,9 +1,14 @@
 import {FooterProps} from "./Footer.props";
+import styles from './Footer.module.css';
+import cn from 'classnames';
+import {format} from 'date-fns';
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return <>
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                1817 - {format(new Date(), 'yyyy')}
+            </div>
+        </footer>
     </>;
 };
