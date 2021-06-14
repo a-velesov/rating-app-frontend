b2f774129dbe5b0d0b4ee4f6cc61ddd9b72133ab
Alexsandr <alexnem@inbox.ru>
2021-06-14 20:58:27 +0300

    sidebar refactor

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,10 @@
 import {SidebarProps} from "./Sidebar.props";
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
 import {Menu} from "../Menu/Menu";
 
-export const Sidebar = ({...props}: SidebarProps): JSX.Element => {
-    return <>
-        <div {...props}>
+export const Sidebar = ({ className,...props}: SidebarProps): JSX.Element => {
+    return <div className={cn(className, styles.sidebar)} {...props}>
             <Menu />
-        </div>
-    </>;
+        </div>;
 };
