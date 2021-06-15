9d144daf9b33d194f12713ca8fbd6f71b240cbdb
Alexsandr <a.velesov@devkeeper.ru>
2021-06-15 17:08:02 +0300

    add Card && Hhdata components

---
+++ components/HhData/Hhdata.tsx
@@ -0,0 +1,45 @@
+import cn from 'classnames';
+import styles from './Hhdata.module.css';
+import {Card} from "../Card/Card";
+import RateIcon from './rate.svg';
+import {HhData} from "../../interfaces/page.interface";
+
+export const Hhdata = ({count, juniorSalary, middleSalary, seniorSalary}: HhData): JSX.Element => {
+    return (
+        <div className={styles.hh}>
+            <Card className={styles.count}>
+                <div className={styles.title}>Всего вакансий</div>
+                <div className={styles.countValue}>{count}</div>
+            </Card>
+            <Card className={styles.salary}>
+                <div>
+                    <div className={styles.title}>Начальный</div>
+                    <div className={styles.salaryValue}>{juniorSalary}</div>
+                    <div className={styles.rate}>
+                        <RateIcon className={styles.filled} />
+                        <RateIcon />
+                        <RateIcon />
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.title}>Средний</div>
+                    <div className={styles.salaryValue}>{middleSalary}</div>
+                    <div className={styles.rate}>
+                        <RateIcon className={styles.filled} />
+                        <RateIcon className={styles.filled} />
+                        <RateIcon />
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.title}>Профессионал</div>
+                    <div className={styles.salaryValue}>{seniorSalary}</div>
+                    <div className={styles.rate}>
+                        <RateIcon className={styles.filled} />
+                        <RateIcon className={styles.filled} />
+                        <RateIcon className={styles.filled} />
+                    </div>
+                </div>
+            </Card>
+        </div>
+    );
+};
--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,5 @@ export * from './Button/Button';
 export * from './Paragraph/Paragraph';
 export * from './Rating/Rating';
 export * from './Tag/Tag';
+export * from './Card/Card';
+export * from './HhData/Hhdata';
